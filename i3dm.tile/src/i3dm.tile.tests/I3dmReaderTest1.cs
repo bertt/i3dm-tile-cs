c61c4b37ac2004a646cf404f3e061e9676031ab9
Bert Temme <btemme@hotmail.com>
2020-06-18 15:26:27 +0200

    more reading i3dms

--- i3dm.tile/src/i3dm.tile.tests/I3dmReaderTest1.cs
+++ i3dm.tile/src/i3dm.tile.tests/I3dmReaderTest1.cs
@@ -1,5 +1,6 @@
 using I3dm.Tile;
 using NUnit.Framework;
+using SharpGLTF.Validation;
 using System.IO;
 using System.Linq;
 
@@ -7,15 +8,87 @@ namespace i3dm.tile.tests
 {
     public class Tests
     {
-        Stream i3dmfile;
         string expectedMagicHeader = "i3dm";
         int expectedVersionHeader = 1;
+
+
+        [Test]
+        public void InstancedWithBatchTableTest()
+        {
+            // arrange
+            // source: http://vcities.umsl.edu/Cesium1.54/Apps/SampleData/Cesium3DTiles/Instanced/InstancedWithBatchTable/
+            var i3dmfile = File.OpenRead(@"testfixtures/instancedWithBatchTable.i3dm");
+            Assert.IsTrue(i3dmfile != null);
+            // act
+            var i3dm = I3dmReader.ReadI3dm(i3dmfile);
+            Assert.IsTrue(expectedMagicHeader == i3dm.I3dmHeader.Magic);
+            Assert.IsTrue(expectedVersionHeader == i3dm.I3dmHeader.Version);
+            Assert.IsTrue(i3dm.I3dmHeader.GltfFormat == 1);
+            Assert.IsTrue(i3dm.BatchTableJson.Length >= 0);
+            Assert.IsTrue(i3dm.BatchTableJson == "{\"Height\":[20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20]} ");
+            Assert.IsTrue(i3dm.GlbData.Length > 0);
+            Assert.IsTrue(i3dm.FeatureTableBinary.Length == 304);
+            var stream = new MemoryStream(i3dm.GlbData);
+            var glb = SharpGLTF.Schema2.ModelRoot.ReadGLB(stream);
+            glb.SaveGLB(@"d:\aaa\instancedwithbatchtable.glb");
+        }
+
+        [Test]
+        public void InstancedOrientationTest()
+        {
+            // arrange
+            // source: http://vcities.umsl.edu/Cesium1.54/Apps/SampleData/Cesium3DTiles/Instanced/InstancedOrientation/
+            var i3dmfile = File.OpenRead(@"testfixtures/instancedOrientation.i3dm");
+            Assert.IsTrue(i3dmfile != null);
+            // act
+            var i3dm = I3dmReader.ReadI3dm(i3dmfile);
+            Assert.IsTrue(expectedMagicHeader == i3dm.I3dmHeader.Magic);
+            Assert.IsTrue(expectedVersionHeader == i3dm.I3dmHeader.Version);
+            Assert.IsTrue(i3dm.I3dmHeader.GltfFormat == 1);
+            Assert.IsTrue(i3dm.BatchTableJson.Length >= 0);
+            Assert.IsTrue(i3dm.GlbData.Length > 0);
+            Assert.IsTrue(i3dm.BatchTableJson == "{\"Height\":[20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20]} ");
+            Assert.IsTrue(i3dm.FeatureTableBinary.Length == 904);
+            var stream = new MemoryStream(i3dm.GlbData);
+            var glb = SharpGLTF.Schema2.ModelRoot.ReadGLB(stream);
+        }
+
+        [Test]
+        public void TreeBillboardTest()
+        {
+            // arrange
+            var i3dmfile = File.OpenRead(@"testfixtures/tree_billboard.i3dm");
+            Assert.IsTrue(i3dmfile != null);
+            // act
+            var i3dm = I3dmReader.ReadI3dm(i3dmfile);
+            Assert.IsTrue(expectedMagicHeader == i3dm.I3dmHeader.Magic);
+            Assert.IsTrue(expectedVersionHeader == i3dm.I3dmHeader.Version);
+            Assert.IsTrue(i3dm.I3dmHeader.GltfFormat == 1);
+            Assert.IsTrue(i3dm.BatchTableJson.Length >= 0);
+            Assert.IsTrue(i3dm.GlbData.Length > 0);
+            Assert.IsTrue(i3dm.FeatureTableBinary.Length == 304);
+            Assert.IsTrue(i3dm.BatchTableJson == "{\"Height\":[20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20]} ");
+            Assert.IsTrue(i3dm.FeatureTableBinary.Length == 304);
+            var stream = new MemoryStream(i3dm.GlbData);
+            try
+            {
+                var glb = SharpGLTF.Schema2.ModelRoot.ReadGLB(stream);
+                //Assert.IsTrue(glb.Asset.Version.Major == 2.0);
+                //Assert.IsTrue(glb.Asset.Generator == "COLLADA2GLTF");
+            }
+            catch(LinkException le)
+            {
+                // we expect linkexception because SharpGLTF does not support KHR_techniques_webgl'
+                Assert.IsTrue(le != null);
+                Assert.IsTrue(le.Message == "ModelRoot Extensions: KHR_techniques_webgl");
+            }
+        }
 
         [Test]
         public void TreeTest()
         {
             // arrange
-            i3dmfile = File.OpenRead(@"testfixtures/tree.i3dm");
+            var i3dmfile = File.OpenRead(@"testfixtures/tree.i3dm");
             Assert.IsTrue(i3dmfile != null);
             // act
             var i3dm = I3dmReader.ReadI3dm(i3dmfile);
@@ -41,7 +114,7 @@ namespace i3dm.tile.tests
         public void CubeTest()
         {
             // arrange
-            i3dmfile = File.OpenRead(@"testfixtures/cube.i3dm");
+            var i3dmfile = File.OpenRead(@"testfixtures/cube.i3dm");
             Assert.IsTrue(i3dmfile != null);
             // act
             var i3dm = I3dmReader.ReadI3dm(i3dmfile);
